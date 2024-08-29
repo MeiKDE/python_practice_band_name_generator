65c5a7ad3fb4a70cab5c0aa2f7aba3cc4fc8b672
Mei Zhang <rubymxzhang@gmail.com>
2024-08-29 13:53:15 +0800

    remove unncessary code and updated README file

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ city = input("What city did you grow up in?\n ")
 pet = input("Give a name of a pet\n ")
 
 #4. Combine the name of their city and pet and show them their band name.
-
-#print("Your band name could be " + city + " " + pet)
-
 print(f"Your band name could be {city} {pet}")
 
 #5. Make sure the input cursor shows on a new line:
